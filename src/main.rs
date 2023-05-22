2e8b4a2b8ce6bf0b0158a6419efc7b83258438dd
Augustin BOUVEROT <abouverot@hackuity.io>
2023-05-22 15:56:36 +0200

    Basic HTTP proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::SocketAddr};
+use tower::make::Shared;
+use hyper::{service::{service_fn}, Server, Request, Body, Response, Client};
+
+async fn handle(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    println!("{:?}", req);
+    let client = Client::new();
+    client.request(req).await
+}
+
+#[tokio::main]
+async fn main() {
+    let make_service = Shared::new(service_fn(handle));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let server = Server::bind(&addr).serve(make_service);
+
+    if let Err(e) = server.await {
+        panic!("Error initializing the HTTP server: {}", e);
+    }
 }
